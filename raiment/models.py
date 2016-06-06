e98dc43f7e79ebf0a018ce27b11aa2f14c64f282
Whitney <whitney@Dales-MacBook.local>
2016-06-06 18:30:07 -0500

    Add post model.

--- raiment/models.py
+++ raiment/models.py
@@ -1,5 +1,18 @@
 from __future__ import unicode_literals
+from django.utils import timezone
 
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200, null=True)
+    text = models.TextField(null=True)
+    image = models.ImageField(null=True)
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
