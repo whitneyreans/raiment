02796dfe25b312dc3e653675c0ba5f302a66e417
Whitney <whitney@Dales-MacBook.local>
2016-07-01 12:52:50 -0500

    Change login url, add login required decorator.

--- mysite/settings.py
+++ mysite/settings.py
@@ -17,6 +17,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = '/login'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
--- raiment/views.py
+++ raiment/views.py
@@ -1,18 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
-
-
-def index(request):
-    return render(request, 'raiment/index.html')
-
-def community(request):
-	return render(request, 'raiment/community.html')
-
-
-def mood(request):
-	return render(request, 'raiment/mood.html')
 
 def signup(request):
 	return render(request, 'registration/signup.html')
@@ -25,5 +15,18 @@ def create_user(request):
 	user = User.objects.create_user(username= new_username,
                                  email= new_email,
                                  password= new_password)
+	user.save()
 
 	return render(request,'raiment/index.html')
+
+@login_required
+def index(request):
+    return render(request, 'raiment/index.html')
+
+def community(request):
+	return render(request, 'raiment/community.html')
+
+
+def mood(request):
+	return render(request, 'raiment/mood.html')
+
